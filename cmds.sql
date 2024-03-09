ac2d44b2599eb4d550b3c0ebb2d75db2b57712a9
Adithya <77010225+adithya1770@users.noreply.github.com>
2024-03-09 12:57:51 +0530

    Add files via upload

---
+++ cmds.sql
@@ -0,0 +1,7 @@
+"insert into creds values('{}','{}')".format(sign_n,sign_p)
+"select * from creds;"
+"insert into login values('{}')".format(n)
+"UPDATE creds SET password =%s WHERE name =%s;"
+"DELETE FROM creds WHERE name=%s;"
+"SELECT password FROM CREDS WHERE name=%s;"
+"SELECT COUNT(*) FROM login;"
